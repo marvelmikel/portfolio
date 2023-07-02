94a447ded787e07674028a5aeb00fb15d13114e9
marvelmikel <marvelmikel1997@gmail.com>
2023-07-02 17:19:45 +0100

    email js integrated

---
+++ src/constants/index.js
@@ -0,0 +1,266 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Flutter Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Blockchain Developer ",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Senior OOP PHP Laravel Lead Developer",
+    company_name: "Humucare Agency",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "April 2022 - Current",
+    points: [
+      "Developing and Maintaining Laravel Applications: My primary responsibility is to develop and maintain web applications using the Laravel framework. This includes designing and implementing application architecture, writing clean and efficient code, and ensuring the application follows Laravel's best practices and conventions.",
+      "Designing and Implementing APIs: Laravel is known for its robust API development capabilities. As a Laravel Developer, am responsible for designing and implementing APIs to expose functionality and data to external systems or for building client applications. This includes defining API endpoints, handling authentication and authorization, and ensuring data security and integrity.",
+      "Implementing Business Logic and Functionality: As a Laravel Developer, you'll be responsible for implementing the business logic and functionality of the web application. This involves working closely with stakeholders, understanding their requirements, and translating them into code. You'll create database models, define relationships, handle data validation and manipulation, and implement complex business rules and algorithms.",
+      "Troubleshooting and Bug Fixing: Another important responsibility is to identify and resolve issues, bugs, and errors in the Laravel application. This involves analyzing and debugging code, writing unit tests, and ensuring the application's stability and performance. You'll work closely with the Quality Assurance (QA) team and stakeholders to address any reported issues promptly."
+     
+    ],
+  },
+  {
+    title: "Senoir Laravel Developer",
+    company_name: "Ashpath",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "Aug 2015 - Jan 2017",
+    points: [
+      "Architecting and Designing Complex Systems: As a senior developer, you'll be responsible for designing and architecting complex systems using the Laravel framework. This involves analyzing project requirements, proposing scalable and maintainable solutions, defining application architecture, and ensuring adherence to best practices and coding standards.",
+      "Mentoring and Leading Junior Developers: Senior Laravel Developers often play a mentoring role, providing guidance and support to junior developers on the team. You'll be responsible for sharing your expertise, reviewing code, providing constructive feedback, and helping junior developers grow their skills and knowledge in Laravel development.",
+      "Performance Optimization and Scalability: As a Senior Laravel Developer, you'll be responsible for optimizing the performance and scalability of Laravel applications. This includes analyzing and improving database queries, optimizing code execution, implementing caching strategies, and leveraging other Laravel features to enhance application performance and handle increased user loads.",
+      "Code Review and Quality Assurance: You'll be responsible for conducting code reviews to ensure code quality, adherence to coding standards, and best practices. This involves reviewing code for potential bugs, security vulnerabilities, and performance bottlenecks. You'll also collaborate with Quality Assurance (QA) teams to ensure proper testing and validation of the Laravel applications.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+    company_name: "Tripmata",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "Feb 2020 - March 2021",
+    points: [
+      "Developing Full Stack Applications: My primary responsibility is to develop full stack applications using the Node.js runtime on the backend and React on the frontend. This involves designing and implementing server-side logic, handling data storage and retrieval, and creating responsive user interfaces using React components.",
+      "Implementing RESTful APIs and GraphQL: You'll be responsible for designing and implementing RESTful APIs or GraphQL endpoints to expose functionality and data to client applications. This includes defining API routes, handling authentication and authorization, validating input data, and ensuring secure communication between the frontend and backend.",
+      "Building Scalable and Maintainable Backend Systems: With NestJS, a framework for building efficient server-side applications, you'll be responsible for architecting and developing scalable and maintainable backend systems. This includes designing the application's modules, services, controllers, and database interactions, and leveraging TypeScript to ensure type safety and code maintainability.",
+      "Ensuring High-Quality Code and Testing: As a Full Stack Developer, you'll be responsible for writing clean, well-structured, and maintainable code. This includes following coding best practices, documenting your code, and performing thorough testing, including unit tests, integration tests, and end-to-end tests, to ensure the reliability and quality of the application.",
+    ],
+  },
+  {
+    title: "PHP OOP Laravel Developer",
+    company_name: "Saucy & Swift ",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "Aug 2017 - Present",
+    points: [
+      "Developing and Maintaining Laravel Applications: primary responsibility is to develop and maintain web applications using the Laravel framework. This includes designing and implementing application architecture, writing clean and efficient code, and ensuring the application follows Laravel's best practices and conventions.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Scaling projects based on client feedback",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Ashpath",
+    description:
+      "Ashpath is a Web-based platform and most secure payment method from a counterparty risk perspective - safeguarding both buyer and seller, all funds transacted using escrow are kept in trust.",
+    tags: [
+      {
+        name: "laravel livewire",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mysql database",
+        color: "green-text-gradient",
+      },
+      {
+        name: "https://ashpath.com/",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://ashpath.com/",
+  },
+  {
+    name: "Tripmata",
+    description:
+      "Web application that enables users to search for Homes or Hotels in a few clicks close to their current location.",
+    tags: [
+      {
+        name: "Reactjs & Nesjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "https://tripmata.com",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://tripmata.com",
+  },
+  {
+    name: "Race Learning",
+    description:
+      "A comprehensive Web application platform for anyone seeking to build a profitable career and business.",
+    tags: [
+      {
+        name: "Mysql Database",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Laravel Tall Stack",
+        color: "green-text-gradient",
+      },
+      {
+        name: "https://racelearning.net/",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://racelearning.net/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
